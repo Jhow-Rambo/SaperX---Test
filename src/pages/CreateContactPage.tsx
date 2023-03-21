68557171bb26cc63dca65dbff67b94a417f4cc2c
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-03-21 11:28:30 -0300

    feat: create 'CreateContact' page

---
+++ src/pages/CreateContactPage.tsx
@@ -0,0 +1,12 @@
+import ContactForm from "../components/ContactForm";
+import { FunctionComponent, Fragment } from "react";
+
+const CreateContact: FunctionComponent = () => {
+  return (
+    <Fragment>
+      <ContactForm />
+    </Fragment>
+  );
+};
+
+export default CreateContact;
