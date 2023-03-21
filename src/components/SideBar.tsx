b677d9b8a0ce338c0fde375a42d55070ceb0ae7d
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-03-21 11:26:59 -0300

    refactor: add Link to side bar icons and add responsive width to the bar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -6,18 +6,33 @@ import PersonAddAltIcon from "@mui/icons-material/PersonAddAlt";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
 import SettingsIcon from "@mui/icons-material/Settings";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 const SideBar: FunctionComponent = () => {
+
+  const currentLocation = useLocation();
+
+  const isInThisRoute = (route: string) => {
+    return route === currentLocation.pathname && "bg-[#E7EAEE]";
+  }
   return (
-    <div className="w-32 h-screen left-0 bg-[#F8F9FB] items-center flex flex-col py-6 justify-between absolute">
+    <div className="md:w-32 w-20 h-full left-0 bg-[#F8F9FB] items-center flex flex-col py-6 justify-between absolute">
       <div className="flex flex-col items-center w-full">
         <Link to="/">
-          <img src={logo} alt="" width={32} height={32} className="mb-10"/>
+          <img src={logo} alt="" width={32} height={32} className="mb-10" />
         </Link>
         <div className="flex flex-col items-center space-y-7">
           <p className="text-[#64748B] font-medium">Menu</p>
-          <DashboardIcon className="cursor-pointer text-[#434343]" />
-          <PersonAddAltIcon className="cursor-pointer text-[#434343]" />
+          <div className={`w-full h-12 ${isInThisRoute("/contacts")} flex items-center justify-center rounded-md`}>
+            <Link to="/contacts">
+              <DashboardIcon className="cursor-pointer text-[#434343]" />
+            </Link>
+          </div>
+          <div className={`w-full h-12 ${isInThisRoute("/create-contact")} flex items-center justify-center rounded-md`}>
+            <Link to="/create-contact">
+              <PersonAddAltIcon className="cursor-pointer text-[#434343]" />
+            </Link>
+          </div>
         </div>
       </div>
       <div className="w-full flex flex-col items-center space-y-7">
