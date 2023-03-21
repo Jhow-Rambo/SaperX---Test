1927918ac70c51d07362a5d0f34b192c9e171da5
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-03-21 11:25:25 -0300

    feat: create contacts and create-contact routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from './pages/Home';
+import ContactsPage from "./pages/Contacts";
 import SideBar from './components/SideBar';
+import CreateContactPage from "./pages/CreateContactPage";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <SideBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/contacts" element={<ContactsPage />} />
+          <Route path="/create-contact" element={<CreateContactPage />} />
         </Routes>
       </BrowserRouter>
     </div>
