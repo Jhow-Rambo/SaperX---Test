93a071c49654c27e1ee5de85ac67f4ccd35e83d4
Jhow-Rambo <jhonatan.ramboo@hotmail.com>
2023-03-21 11:27:58 -0300

    feat: create Contacts page

---
+++ src/pages/Contacts.tsx
@@ -0,0 +1,12 @@
+import Contacts from "../components/Contacts";
+import { FunctionComponent, Fragment } from "react";
+
+const HomePage: FunctionComponent = () => {
+  return (
+    <Fragment>
+      <Contacts />
+    </Fragment>
+  );
+};
+
+export default HomePage;
